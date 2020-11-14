d38568221654caf2d247b61e385fc49355e55931
BanuTorun <banu.torun@hotmail.com>
2020-11-13 23:35:39 -0600

    added project

---
+++ target/timeline-report/report.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "fb127a2e-943e-4955-b6df-798f59c7620a",
+    "feature": "As user, I want to be able to create calendar events",
+    "scenario": "Create calendar event with default time",
+    "start": 1605236636403,
+    "group": 12,
+    "content": "",
+    "tags": "@calendar_events,@smoke,",
+    "end": 1605236665332,
+    "className": "passed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 12,
+    "content": "Thread[pool-2-thread-1,5,main]"
+  }
+]);
+});
